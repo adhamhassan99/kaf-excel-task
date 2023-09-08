b098421a554aa06ce95b16c61bd6798ed4890036
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-09-08 22:11:17 +0300

    fix color issue

--- src/components/StatisticsDetail.tsx
+++ src/components/StatisticsDetail.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function StatisticsDetail({ title, value, textColor }: Props) {
   return (
-    <div className={`flex justify-between px-1 text-${textColor}`}>
+    <div className={`flex justify-between px-1  text-${textColor}`}>
       <div className="text-2xl">{title}</div>
       <div className="text-2xl">{value}</div>
     </div>
